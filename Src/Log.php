3f5fdc00282651b722dcda79e1d1636fe2a8dd88
Vladimir Savin <wowabbs@gmail.com>
2026-09-17 04:32:26 +0400

    App: Added custom logfile

--- Src/Log.php
+++ Src/Log.php
@@ -5,8 +5,8 @@ for($F=__FILE__; $F;) if(@include($F=DirName($F)).'/Using.php') break;
 
 //$Loader->GetLogger()->Add($argv[0].'.log'); 
 $info=PathInfo($argv[0]); //$_SERVER['SCRIPT_FILENAME'] instead
-$logFile=$info['dirname'].'/'.$info['filename'];
-$Loader->GetLogger()->Add($logFile.'.log');
+$logFile??=$info['dirname'].'/'.$info['filename'].'.log';
+$Loader->GetLogger()->Add($logFile);
 $log=$Loader->GetLogger()->Log(...);
 
 Function Log(...$Args)
