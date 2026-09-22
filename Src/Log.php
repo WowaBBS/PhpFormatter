8ed1319a7e386d5adfdba9d86b1ce1cc8fbfc21a
Vladimir Savin <wowabbs@gmail.com>
2026-09-23 03:55:50 +0400

    Update using WLib

--- Src/Log.php
+++ Src/Log.php
@@ -3,10 +3,7 @@ namespace Reformat;
 
 for($F=__FILE__; $F;) if(@include($F=DirName($F)).'/Using.php') break;
 
-//$Loader->GetLogger()->Add($argv[0].'.log'); 
-$info=PathInfo($argv[0]); //$_SERVER['SCRIPT_FILENAME'] instead
-$logFile??=$info['dirname'].'/'.$info['filename'].'.log';
-$Loader->GetLogger()->Add($logFile);
+$Loader->GetLogger()->ScriptLog($LogFile); 
 $log=$Loader->GetLogger()->Log(...);
 
 Function Log(...$Args)
